1c3f82d8ab646c0a94e6281860a72524e8690f81
Passakorn Chantarapakorn <passakornchantarapakorn@Passakorns-MacBook-Air.local>
2024-01-09 03:15:56 +0700

    Edit: question-8

--- question-8.js
+++ question-8.js
@@ -1,12 +1,16 @@
 // Question #8: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
-try {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
-  if (!res.ok) throw new Error("Network response was not ok.");
-  const users = await res.json();
-  const userNames = users.map((name) => name.name);
-  console.log(userNames);
-} catch (error) {
-  console.error("There was a problem fetching the user data:", error);
+async function getUserNames() {
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    if (!res.ok) throw new Error("Network response was not ok.");
+    const users = await res.json();
+    const userNames = users.map((name) => name.name);
+    console.log(userNames);
+  } catch (error) {
+    console.error("There was a problem fetching the user data:", error);
+  }
 }
+
+getUserNames();
